de3263c6380f45da9ab17ebb3a56691304669381
Mylavarapu Sarath Sai <sarath@advenio.tech>
2023-01-13 07:17:43 +0530

    with individual states

--- src/components/AnimalExample.tsx
+++ src/components/AnimalExample.tsx
@@ -3,30 +3,51 @@ import React, { useState, useEffect } from 'react'
 
 const useFetchDog = () => {
  const [dog, setDog] = useState<string>()
+ const [isLoadingDog, setIsLoading] = useState<Boolean>(false)
+ const [isErrorDog, setIsError] = useState<Boolean>(false)
  const initFetchDog = async () => {
-  const response = await fetchDog()
-  setDog(response.data.message)
+  try {
+   setIsLoading(true)
+   const response = await fetchDog()
+   isErrorDog && setIsError(false)
+   setDog(response.data.message)
+   setIsLoading(false)
+  } catch (err) {
+   setIsError(true)
+   console.log('Error while loading dog', err)
+  }
  }
 
  return {
   dog,
+  isErrorDog,
+  isLoadingDog,
   initFetchDog,
  }
 }
 
 const useFetchCat = () => {
  const [cat, setCat] = useState<string>()
+ const [isLoadingCat, setIsLoading] = useState<Boolean>(false)
+ const [isErrorCat, setIsError] = useState<Boolean>(false)
  const initFetchCat = async () => {
-  const response = await fetchCat()
-
-  setCat((response.data && response.data[0].url) || '')
+  try {
+   setIsLoading(true)
+   const response = await fetchCat()
+   isErrorCat && setIsError(false)
+   setCat((response.data && response.data[0].url) || '')
+   setIsLoading(false)
+  } catch (err) {
+   console.log('Error while loading cat', err)
+   setIsError(true)
+  }
  }
- return { cat, initFetchCat }
+ return { cat, isErrorCat, isLoadingCat, initFetchCat }
 }
 
 const useFetchAnimals = () => {
- const { dog, initFetchDog } = useFetchDog()
- const { cat, initFetchCat } = useFetchCat()
+ const { dog, isErrorDog, isLoadingDog, initFetchDog } = useFetchDog()
+ const { cat, isErrorCat, isLoadingCat, initFetchCat } = useFetchCat()
 
  const fetchAnimals = () => {
   initFetchDog()
@@ -38,7 +59,11 @@ const useFetchAnimals = () => {
 
  return {
   dog,
+  isErrorDog,
+  isLoadingDog,
   cat,
+  isErrorCat,
+  isLoadingCat,
   fetchAnimals,
  }
 }
@@ -69,16 +94,35 @@ const styles = {
 }
 
 const AnimalExample = () => {
- const { dog, cat, fetchAnimals } = useFetchAnimals()
+ const {
+  dog,
+  isErrorDog,
+  isLoadingDog,
+  cat,
+  isErrorCat,
+  isLoadingCat,
+  fetchAnimals,
+ } = useFetchAnimals()
  return (
   <main style={styles.container}>
    <div style={styles.imgsContainer}>
     <div style={styles.imgContainer}>
-     {cat ? <img src={cat} style={styles.img} /> : null}
+     {isErrorDog ? (
+      <p>Problem Doggo Appeared!</p>
+     ) : isLoadingDog ? (
+      <p>Loading Dog</p>
+     ) : dog ? (
+      <img src={dog} style={styles.img} />
+     ) : null}
     </div>
     <div style={styles.imgContainer}>
-     {' '}
-     {dog ? <img src={dog} style={styles.img} /> : null}
+     {isErrorCat ? (
+      <p>Problem Cate Appeared!</p>
+     ) : isLoadingCat ? (
+      <p>Loading Cat</p>
+     ) : cat ? (
+      <img src={cat} style={styles.img} />
+     ) : null}
     </div>
    </div>
    <button onClick={fetchAnimals} style={styles.button}>
